3b38f2ca7c236fb16c0bfca0f214093118c98aa9
user <user@mb-923346.local>
2023-01-24 17:19:27 +0200

    Init project

---
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by user on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
