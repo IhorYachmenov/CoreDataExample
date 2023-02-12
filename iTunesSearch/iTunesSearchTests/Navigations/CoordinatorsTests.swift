9b676b338efccfbc496d71dff32df67c7049f122
Ihor Yachmenov <Ihor.Yachmenov@gmail.com>
2023-02-13 00:16:27 +0200

    Created Router and Coordinator

---
+++ iTunesSearch/iTunesSearchTests/Navigations/CoordinatorsTests.swift
@@ -0,0 +1,8 @@
+//
+//  CoordinatorsTests.swift
+//  iTunesSearchTests
+//
+//  Created by user on 12.02.2023.
+//
+
+import Foundation
---
+++ iTunesSearch/iTunesSearchTests/Navigations/RouterMock.swift
@@ -0,0 +1,8 @@
+//
+//  RouterMock.swift
+//  iTunesSearchTests
+//
+//  Created by user on 12.02.2023.
+//
+
+import Foundation
