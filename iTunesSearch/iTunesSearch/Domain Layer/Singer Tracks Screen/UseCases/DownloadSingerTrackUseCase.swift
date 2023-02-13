dfdada70270d30fe9a28dcb2968f4e8eaf6978c6
Ihor Yachmenov <Ihor.Yachmenov@gmail.com>
2023-02-13 14:35:15 +0200

    Updated download key request

--- iTunesSearch/iTunesSearch/Domain Layer/Singer Tracks Screen/UseCases/DownloadSingerTrackUseCase.swift
+++ iTunesSearch/iTunesSearch/Domain Layer/Singer Tracks Screen/UseCases/DownloadSingerTrackUseCase.swift
@@ -16,7 +16,7 @@ extension DownloadSingerTrackUseCase: DownloadSingerTrackUseCaseInterface {
         let request = SearchSingerTrackAPI()
         let apiLoader = APILoader(apiHandler: request)
         print("Singer Name Downloading ->", name)
-        let param: Dictionary<String, Any> = ["limit" : "1", "music" : "media", "term" : name]
+        let param: Dictionary<String, Any> = ["limit" : "1", "media" : "music", "term" : name]
     
         apiLoader.loadAPIRequest(requestData: param) { (model, error) in
             if let _ = error {
