4d464721a35e6ba037b3e7d21de82e1b30f1926f
Ihor Yachmenov <Ihor.Yachmenov@gmail.com>
2023-02-21 13:24:23 +0200

    Fixed errors

--- iTunesSearch/iTunesSearch/Navigations/SingerTracksCoordinator.swift
+++ iTunesSearch/iTunesSearch/Navigations/SingerTracksCoordinator.swift
@@ -25,7 +25,6 @@ final class SingerTracksCoordinator: Coordinator {
         }
     }
 #warning("** Q: - coordinator -> router dependency, inside showDetailCoordinator")
-#warning("check terminal warning")
     private func showDetailCoordinator(trackId: String) {
         //        let router = RounterBuilder.makerRouter(type: .PlayerScreenRouter)
         let detailCoordinator = SingerTrackDetailsCoordinator(router: router, trackId: trackId)
