4223869e7cc17d65931f0d2ebe5568597c40ddf3
Ihor Yachmenov <Ihor.Yachmenov@gmail.com>
2023-03-02 18:40:00 +0200

    Removed all test framework, all framework except data layer connected statically

--- iTunesSearch/Data Layer/PersistentStorage/CoreData/CoreDataStorage/CoreDataStorageManager.swift
+++ iTunesSearch/Data Layer/PersistentStorage/CoreData/CoreDataStorage/CoreDataStorageManager.swift
@@ -13,99 +13,16 @@ final class CoreDataStorageManager {
 
     init() {}
     
-    private func createEntity() -> NSManagedObjectModel {
-        let singerTrackEntity = NSEntityDescription()
-        singerTrackEntity.name = "SingerTrack"
-        singerTrackEntity.managedObjectClassName = NSStringFromClass(SingerTrack.self)
-        
-        let trackPriceAttribute = NSAttributeDescription()
-        trackPriceAttribute.name = "trackPrice"
-        trackPriceAttribute.attributeType = .stringAttributeType
-        trackPriceAttribute.isOptional = false
-        
-        let trackNameAttribute = NSAttributeDescription()
-        trackNameAttribute.name = "trackName"
-        trackNameAttribute.attributeType = .stringAttributeType
-        trackNameAttribute.isOptional = false
-        
-        let trackImgURLAttribute = NSAttributeDescription()
-        trackImgURLAttribute.name = "trackImgURL"
-        trackImgURLAttribute.attributeType = .stringAttributeType
-        trackImgURLAttribute.isOptional = false
-        
-        let trackIdAttribute = NSAttributeDescription()
-        trackIdAttribute.name = "trackId"
-        trackIdAttribute.attributeType = .stringAttributeType
-        trackIdAttribute.isOptional = false
-        
-        let singerNameAttribute = NSAttributeDescription()
-        singerNameAttribute.name = "singerName"
-        singerNameAttribute.attributeType = .stringAttributeType
-        singerNameAttribute.isOptional = false
-        
-        let releaseDateAttribute = NSAttributeDescription()
-        releaseDateAttribute.name = "releaseDate"
-        releaseDateAttribute.attributeType = .stringAttributeType
-        releaseDateAttribute.isOptional = false
-        
-        let genreAttribute = NSAttributeDescription()
-        genreAttribute.name = "genre"
-        genreAttribute.attributeType = .stringAttributeType
-        genreAttribute.isOptional = false
-        
-        let demoURLAttribute = NSAttributeDescription()
-        demoURLAttribute.name = "demoURL"
-        demoURLAttribute.attributeType = .stringAttributeType
-        demoURLAttribute.isOptional = false
-        
-        let countryAttribute = NSAttributeDescription()
-        countryAttribute.name = "country"
-        countryAttribute.attributeType = .stringAttributeType
-        countryAttribute.isOptional = false
-        
-        let collectionPriceAttribute = NSAttributeDescription()
-        collectionPriceAttribute.name = "collectionPrice"
-        collectionPriceAttribute.attributeType = .stringAttributeType
-        collectionPriceAttribute.isOptional = false
-        
-        let collectionNameAttribute = NSAttributeDescription()
-        collectionNameAttribute.name = "collectionName"
-        collectionNameAttribute.attributeType = .stringAttributeType
-        collectionNameAttribute.isOptional = false
-        
-        singerTrackEntity.properties = [
-            trackPriceAttribute,
-            trackNameAttribute,
-            trackImgURLAttribute,
-            trackIdAttribute,
-            singerNameAttribute,
-            releaseDateAttribute,
-            genreAttribute,
-            demoURLAttribute,
-            countryAttribute,
-            collectionPriceAttribute,
-            collectionNameAttribute]
-
-        let model = NSManagedObjectModel()
-        model.entities = [singerTrackEntity]
-        
-        return model
-    }
-
     private lazy var persistentContainer: NSPersistentContainer = {
-//        let bundle = Bundle(for: CoreDataStorageManager.self)
-
-//        let bundle = Bundle.main
-//
-//        guard
-//            let modelUrl = bundle.url(forResource: "SingerTrackDataModel", withExtension: "momd"),
-//            let modelUrl = bundle.url(forResource: "SingerTrackDataModelB", withExtension: "momd"),
-//            let model = NSManagedObjectModel(contentsOf: modelUrl)
-//        else {
-//            fatalError("No model")
-//        }
+        let bundle = Bundle(for: CoreDataStorageManager.self)
         
-        let model = createEntity()
+        guard
+            let modelUrl = bundle.url(forResource: "SingerTrackDataModel", withExtension: "momd"),
+            let model = NSManagedObjectModel(contentsOf: modelUrl)
+        else {
+            fatalError("No model")
+        }
+        
         let container = NSPersistentContainer(name: "Container", managedObjectModel: model)
         container.loadPersistentStores { _, error in
             if let error = error as NSError? {
@@ -127,3 +44,84 @@ final class CoreDataStorageManager {
     }
 }
 
+
+
+// MARK: - programmatically created entity
+//private func createEntity() -> NSManagedObjectModel {
+//    let singerTrackEntity = NSEntityDescription()
+//    singerTrackEntity.name = "SingerTrack"
+//    singerTrackEntity.managedObjectClassName = NSStringFromClass(SingerTrack.self)
+//
+//    let trackPriceAttribute = NSAttributeDescription()
+//    trackPriceAttribute.name = "trackPrice"
+//    trackPriceAttribute.attributeType = .stringAttributeType
+//    trackPriceAttribute.isOptional = false
+//
+//    let trackNameAttribute = NSAttributeDescription()
+//    trackNameAttribute.name = "trackName"
+//    trackNameAttribute.attributeType = .stringAttributeType
+//    trackNameAttribute.isOptional = false
+//
+//    let trackImgURLAttribute = NSAttributeDescription()
+//    trackImgURLAttribute.name = "trackImgURL"
+//    trackImgURLAttribute.attributeType = .stringAttributeType
+//    trackImgURLAttribute.isOptional = false
+//
+//    let trackIdAttribute = NSAttributeDescription()
+//    trackIdAttribute.name = "trackId"
+//    trackIdAttribute.attributeType = .stringAttributeType
+//    trackIdAttribute.isOptional = false
+//
+//    let singerNameAttribute = NSAttributeDescription()
+//    singerNameAttribute.name = "singerName"
+//    singerNameAttribute.attributeType = .stringAttributeType
+//    singerNameAttribute.isOptional = false
+//
+//    let releaseDateAttribute = NSAttributeDescription()
+//    releaseDateAttribute.name = "releaseDate"
+//    releaseDateAttribute.attributeType = .stringAttributeType
+//    releaseDateAttribute.isOptional = false
+//
+//    let genreAttribute = NSAttributeDescription()
+//    genreAttribute.name = "genre"
+//    genreAttribute.attributeType = .stringAttributeType
+//    genreAttribute.isOptional = false
+//
+//    let demoURLAttribute = NSAttributeDescription()
+//    demoURLAttribute.name = "demoURL"
+//    demoURLAttribute.attributeType = .stringAttributeType
+//    demoURLAttribute.isOptional = false
+//
+//    let countryAttribute = NSAttributeDescription()
+//    countryAttribute.name = "country"
+//    countryAttribute.attributeType = .stringAttributeType
+//    countryAttribute.isOptional = false
+//
+//    let collectionPriceAttribute = NSAttributeDescription()
+//    collectionPriceAttribute.name = "collectionPrice"
+//    collectionPriceAttribute.attributeType = .stringAttributeType
+//    collectionPriceAttribute.isOptional = false
+//
+//    let collectionNameAttribute = NSAttributeDescription()
+//    collectionNameAttribute.name = "collectionName"
+//    collectionNameAttribute.attributeType = .stringAttributeType
+//    collectionNameAttribute.isOptional = false
+//
+//    singerTrackEntity.properties = [
+//        trackPriceAttribute,
+//        trackNameAttribute,
+//        trackImgURLAttribute,
+//        trackIdAttribute,
+//        singerNameAttribute,
+//        releaseDateAttribute,
+//        genreAttribute,
+//        demoURLAttribute,
+//        countryAttribute,
+//        collectionPriceAttribute,
+//        collectionNameAttribute]
+//
+//    let model = NSManagedObjectModel()
+//    model.entities = [singerTrackEntity]
+//
+//    return model
+//}
